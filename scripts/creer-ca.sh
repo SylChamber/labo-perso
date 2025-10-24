816885167d91695817a1d99cc89d1ec86ff5eba6
Sylvain Chamberland <SylChamber@users.noreply.github.com>
2025-10-24 07:53:08 -0400

    refactor (certificats): créer et stocker les autorités sous ~/.local/state/step

--- scripts/creer-ca.sh
+++ scripts/creer-ca.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Crée des autorités privées de certificat racine et intermédiaire pour le réseau local
-# TODO - créer les autorités sous ~/.local/state/step/certs
 
 set -eo pipefail
 
@@ -11,16 +10,27 @@ fi
 
 SCRIPT_DIR=$(dirname "$0")
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-CERTS_DIR=$REPO_ROOT_DIR/certs
+# Si STEPPATH n'existe pas, définir un dossier par défaut
+STEPPATH=${STEPPATH:-~/.local/state/step}
+CERTS_DIR=$STEPPATH/certs
 # 25 ans
 NOT_AFTER=219144h
 
-mkdir -p $CERTS_DIR
+ROOT_CA_NAME=root_ca
+INTERMEDIATE_CA_NAME=intermediate_ca
 
-if [ ! -f $SCRIPT_DIR/root-tls.json.tpl ] || [ ! -f $SCRIPT_DIR/intermediate-tls.json.tpl ]; then
-    >&2 echo "Les gabarits root-tls.json.tpl et intermediate-tls.json.tpl sont requis."
+if [ ! -d $CERTS_DIR ]; then
+    mkdir -p $CERTS_DIR
+fi
+
+if [ ! -f $SCRIPT_DIR/${ROOT_CA_NAME}.json.tpl ] || [ ! -f $SCRIPT_DIR/${INTERMEDIATE_CA_NAME}.json.tpl ]; then
+    >&2 echo "Les gabarits ${ROOT_CA_NAME}.json.tpl et ${INTERMEDIATE_CA_NAME}.json.tpl sont requis sous $SCRIPT_DIR."
     exit 1
 fi
+
+# copier les gabarits dans $CERTS_DIR; mode interactif au cas où les fichiers existeraient
+>&2 echo "Copie des gabarits dans le dossier de configuration de certificats $CERTS_DIR"
+cp -i $SCRIPT_DIR/*.tpl $CERTS_DIR/
 
 # assurer la présence des paramètres
 
@@ -39,8 +49,8 @@ export INTERMEDIATE_CA_NAME="$ORGANISATION Intermediary Private Certificate Auth
 
 # Générer les gabarits de certificats
 >&2 echo "Génération des gabarits de certificats..."
-cat $SCRIPT_DIR/root-tls.json.tpl | envsubst | tee $CERTS_DIR/root-tls.json
-cat $SCRIPT_DIR/intermediate-tls.json.tpl | envsubst | tee $CERTS_DIR/intermediate-tls.json
+cat $SCRIPT_DIR/${ROOT_CA_NAME}.json.tpl | envsubst | tee $CERTS_DIR/${ROOT_CA_NAME}.json
+cat $SCRIPT_DIR/${INTERMEDIATE_CA_NAME}.json.tpl | envsubst | tee $CERTS_DIR/${INTERMEDIATE_CA_NAME}.json
 
 # Demander les mots de passe des certificats
 read -sp "Veuillez saisir un mot de passe pour le certificat root: " ROOT_TLS_PASSWORD
@@ -51,7 +61,7 @@ if [ -z "$ROOT_TLS_PASSWORD" ]; then
     ROOT_TLS_PASSWORD=$(openssl rand -base64 42)
 fi
 
-echo $ROOT_TLS_PASSWORD > $CERTS_DIR/root-tls.password
+echo $ROOT_TLS_PASSWORD > $CERTS_DIR/${ROOT_CA_NAME}.password
 
 read -sp "Veuillez saisir un mot de passe pour le certificat intermédiaire: " INTERMEDIATE_TLS_PASSWORD
 >&2 echo
@@ -61,45 +71,45 @@ if [ -z "$INTERMEDIATE_TLS_PASSWORD" ]; then
     INTERMEDIATE_TLS_PASSWORD=$(openssl rand -base64 42)
 fi
 
-echo $INTERMEDIATE_TLS_PASSWORD > $CERTS_DIR/intermediate-tls.password
+echo $INTERMEDIATE_TLS_PASSWORD > $CERTS_DIR/${INTERMEDIATE_CA_NAME}.password
 
 # Générer la paire de certificat racine
 >&2 echo "Génération du certificat racine..."
 step certificate create \
   "${ROOT_CA_NAME}" \
-  "$CERTS_DIR/root-tls.crt" \
-  "$CERTS_DIR/root-tls.key" \
-  --template="$CERTS_DIR/root-tls.json" \
+  "$CERTS_DIR/${ROOT_CA_NAME}.crt" \
+  "$CERTS_DIR/${ROOT_CA_NAME}.key" \
+  --template="$CERTS_DIR/${ROOT_CA_NAME}.json" \
   --kty="EC" \
   --curve="P-256" \
-  --password-file="$CERTS_DIR/root-tls.password" \
+  --password-file="$CERTS_DIR/${ROOT_CA_NAME}.password" \
   --not-before="0s" \
   --not-after="$NOT_AFTER" \
   --force
 
-TLS_ROOT_CRT=$(cat $CERTS_DIR/root-tls.crt | sed 's/^/        /')
-TLS_ROOT_KEY=$(cat $CERTS_DIR/root-tls.key | sed 's/^/        /')
-TLS_ROOT_FINGERPRINT=$(step certificate fingerprint $CERTS_DIR/root-tls.crt)
+TLS_ROOT_CRT=$(cat $CERTS_DIR/${ROOT_CA_NAME}.crt | sed 's/^/        /')
+TLS_ROOT_KEY=$(cat $CERTS_DIR/${ROOT_CA_NAME}.key | sed 's/^/        /')
+TLS_ROOT_FINGERPRINT=$(step certificate fingerprint $CERTS_DIR/${ROOT_CA_NAME}.crt)
 
 # Générer la paire de certificat intermédiaire
 >&2 echo "Génération du certificat intermédiaire..."
 step certificate create \
   "${INTERMEDIATE_CA_NAME}" \
-  "$CERTS_DIR/intermediate-tls.crt" \
-  "$CERTS_DIR/intermediate-tls.key" \
-  --template="$CERTS_DIR/intermediate-tls.json" \
+  "$CERTS_DIR/${INTERMEDIATE_CA_NAME}.crt" \
+  "$CERTS_DIR/${INTERMEDIATE_CA_NAME}.key" \
+  --template="$CERTS_DIR/${INTERMEDIATE_CA_NAME}.json" \
   --kty="EC" \
   --curve="P-256" \
-  --password-file="$CERTS_DIR/intermediate-tls.password" \
+  --password-file="$CERTS_DIR/${INTERMEDIATE_CA_NAME}.password" \
   --not-before="0s" \
   --not-after="$NOT_AFTER" \
-  --ca="$CERTS_DIR/root-tls.crt" \
-  --ca-key="$CERTS_DIR/root-tls.key" \
-  --ca-password-file=$CERTS_DIR/"root-tls.password" \
+  --ca="$CERTS_DIR/${ROOT_CA_NAME}.crt" \
+  --ca-key="$CERTS_DIR/${ROOT_CA_NAME}.key" \
+  --ca-password-file="$CERTS_DIR/${ROOT_CA_NAME}.password" \
   --force
 
-TLS_INTERMEDIATE_CRT=$(cat $CERTS_DIR/intermediate-tls.crt | sed 's/^/        /')
-TLS_INTERMEDIATE_KEY=$(cat $CERTS_DIR/intermediate-tls.key | sed 's/^/        /')
+TLS_INTERMEDIATE_CRT=$(cat $CERTS_DIR/${INTERMEDIATE_CA_NAME}.crt | sed 's/^/        /')
+TLS_INTERMEDIATE_KEY=$(cat $CERTS_DIR/${INTERMEDIATE_CA_NAME}.key | sed 's/^/        /')
 
 # Définir un nom de provisioner s'il n'a pas été fourni
 [ -z $PROVISIONER_NAME ] && export PROVISIONER_NAME=admin
@@ -126,6 +136,6 @@ step crypto jwk create \
   --password-file=$CERTS_DIR/jwk_provisioner.password \
   --force
 
->&2 echo "Les fichiers de certificats ont été créés dans le dossier certs.
+>&2 echo "Les fichiers de certificats ont été créés dans le dossier $CERTS_DIR.
 Avant de les supprimer, veuillez les sauvegarder de façon sécuritaire
 et générez le fichier manifeste de secrets pour step-ca."
